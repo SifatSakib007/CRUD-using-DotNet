997cf94236f3b529625bec4baa089d4a0ec2632b
SifatSakib007 <sifatsakib26@gmail.com>
2024-04-24 06:37:48 +0600

    Update Bulky web razor temp

---
+++ BulkyWebRazor_Temp/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace BulkyWebRazor_Temp.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [MaxLength(10, ErrorMessage = "Display Order must be in numbers and in between 1 to 1000")]
+        public string DisplayOrder { get; set; }
+    }
+}
--- BulkyWebRazor_Temp/obj/Debug/net8.0/BulkyWebRazor_Temp.AssemblyInfo.cs
+++ BulkyWebRazor_Temp/obj/Debug/net8.0/BulkyWebRazor_Temp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyWebRazor_Temp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c087a7bf04cfe7e158aea9fb437e79b048598e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyWebRazor_Temp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyWebRazor_Temp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
